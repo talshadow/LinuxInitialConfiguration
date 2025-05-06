a5b65f556b2fc70f1d318e75e70366f169781b4a
tal2 <tal_shadow@ukr.net>
2025-05-06 15:42:07 +0300

    fix parameter issue

--- InstallSteps.sh
+++ InstallSteps.sh
@@ -20,7 +20,7 @@ popd
 
 pushd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb -y
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt --fix-broken install -y
 popd
 
